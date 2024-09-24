67e0b197d96364bf0d9a799faeec7507c8123f31
nguyenquocviet1002 <vietnguyen10022001@gmail.com>
2024-09-24 16:32:59 +0700

    update

--- Popup/popup_knhd_1_0_0/js/popup_knhd_1_0_0.js
+++ Popup/popup_knhd_1_0_0/js/popup_knhd_1_0_0.js
@@ -1,39 +1,89 @@
-const popup_knhd_1_0_0Html = `
-<div class="regis_knhd_1_0_0 modal" id="regis_knhd_1_0_0" style="display: flex;">
-    <div class="modal-bg" onclick="regis_knhd_1_0_0Remove()"></div>
-    <div class="modal-box animate-opacity">
-        <div class="regis_knhd_1_0_0__close modal-close" onclick="regis_knhd_1_0_0Remove()">×</div>
-        <div class="regis_knhd_1_0_0__body">
-            <div class="regis_knhd_1_0_0__title">Quý khách để lại thông tin để nhận ưu đãi tốt nhất</div>
-            <form class="regis_knhd_1_0_0__form">
+const htmlPopup_knhd_1_0_0 = `
+<div class="popup_knhd_1_0_0 modal" id="popup_knhd_1_0_0__modal" style="display: flex;">
+    <div class="modal-bg" onclick="removePopup_knhd_1_0_0()"></div>
+    <div class="popup_knhd_1_0_0__box modal-box animate-opacity">
+        <div class="popup_knhd_1_0_0__close modal-close" onclick="removePopup_knhd_1_0_0()">×</div>
+        <div class="popup_knhd_1_0_0__body">
+            <div class="popup_knhd_1_0_0__title">Chào bạn!</div>
+            <div class="popup_knhd_1_0_0__sub">Bạn cần chúng tôi tư vấn về vấn đề gì?</div>
+            <form id="popup_knhd_1_0_0__form" action="https://hoidap.benhvienthammykangnam.vn/cau-hoi/" method="post" enctype="multipart/form-data" role="form">
                 <div style="display: none;">
-                    <input name="referred" value="">
-                    <input name="code_campaign" value="584304803">
-                    <input name="name_campaign" value="[Kangnam] Thẩm Mỹ Mắt">
-                    <input name="first_link" value="">
-                    <input name="website" value="">
-                    <input name="location" value="">
-                    <input name="itext" value="">
+                    <input name="avatar" value="/wp-content/themes/KnAnser2024/Module/Category/cateList_knhd_1_0_0/images/avatar.png">
                 </div>
-                <div class="regis_knhd_1_0_0__control form-group">
-                    <input type="text" name="iname" class="regis_knhd_1_0_0__input" placeholder="Họ tên">
+                <div class="popup_knhd_1_0_0__control form-group">
+                    <label class="popup_knhd_1_0_0__label">Tiêu đề <span>*</span></label>
+                    <input type="text" class="popup_knhd_1_0_0__input" name="isubject" placeholder="Nhập tiêu đề câu hỏi">
                     <div class="form-message"></div>
                 </div>
-                <div class="regis_knhd_1_0_0__control form-group">
-                    <input type="text" name="imob" class="regis_knhd_1_0_0__input" placeholder="Số điện thoại">
+                <div class="popup_knhd_1_0_0__control form-group">
+                    <label class="popup_knhd_1_0_0__label">Nội dung câu hỏi <span>*</span></label>
+                    <textarea class="popup_knhd_1_0_0__note" name="iquestion" placeholder="Tôi muốn hỏi..." rows="4"></textarea>
                     <div class="form-message"></div>
                 </div>
-                <div class="regis_knhd_1_0_0__control form-group">
-                    <select class="regis_knhd_1_0_0__input">
-                        <option value="">Dịch vụ</option>
+                <div class="popup_knhd_1_0_0__control form-group">
+                    <label class="popup_knhd_1_0_0__label">Chuyên mục tư vấn online <span>*</span></label>
+                    <select class="popup_knhd_1_0_0__input" name="icate">
+                        <option value="">Chọn chuyên mục</option>
                     </select>
                     <div class="form-message"></div>
                 </div>
-                <div class="regis_knhd_1_0_0__submit">
-                    <input type="submit" value="Nhận ưu đãi">
+                <div class="popup_knhd_1_0_0__control form-group">
+                    <label class="popup_knhd_1_0_0__label">Họ và tên <span>*</span></label>
+                    <input type="text" class="popup_knhd_1_0_0__input" name="iname" placeholder="Nhập họ tên">
+                    <div class="form-message"></div>
+                </div>
+                <div class="popup_knhd_1_0_0__submit">
+                    <input type="submit" value="ĐẶT CÂU HỎI">
                 </div>
             </form>
         </div>
     </div>
 </div>
 `;
+
+const removePopup_knhd_1_0_0 = () => {
+    document.querySelector('#popup_knhd_1_0_0__modal').remove();
+}
+
+const removeAPopup_knhd_1_0_0 = (arr, a) => {
+    let what, ax;
+    while ((ax= arr.indexOf(a)) !== -1) {
+        arr.splice(ax, 1);
+    }
+    return arr;
+}
+
+const queryPopup_knhd_1_0_0 = async () => {
+    const response = await fetch('https://hoidap.benhvienthammykangnam.vn/wp-json/wp/v2/categories');
+    const data = await response.json();
+    const cate = data.map(item => item.name);
+    const cateConvert = removeAPopup_knhd_1_0_0(cate, 'Chưa phân loại');
+    return cateConvert;
+}
+
+const btnPopup_knhd_1_0_0 = document.querySelector('.popup_knhd_1_0_0__btn');
+let catePopup_knhd_1_0_0;
+btnPopup_knhd_1_0_0.addEventListener('click', async () => {
+    document.body.insertAdjacentHTML('beforeend', htmlPopup_knhd_1_0_0);
+    // thao tác form    
+    if(!catePopup_knhd_1_0_0) {
+        catePopup_knhd_1_0_0 = await queryPopup_knhd_1_0_0();
+    }
+    let html = '<option value="">Chọn chuyên mục</option>';
+    catePopup_knhd_1_0_0.map(item => {
+        html += `<option value="${item}">${item}</option>`;
+    });
+    document.querySelector('select[name="icate"]').innerHTML = html;
+
+    Validator({
+        form: '#popup_knhd_1_0_0__form',
+        errorSelector: '.form-message',
+        formGroupSelector: '.form-group',
+        rules: [
+            Validator.isRequired('input[name="isubject"]'),
+            Validator.isRequired('input[name="iname"]'),
+            Validator.isRequired('select[name="icate"]'),
+            Validator.isRequired('textarea[name="iquestion"]'),
+        ],
+    });
+})
--- Popup/regis_knhd_1_2_0/js/regis_knhd_1_2_0.js
+++ Popup/regis_knhd_1_2_0/js/regis_knhd_1_2_0.js
@@ -1,13 +1,13 @@
-const btnRegis_knhd_1_0_0 = document.querySelector('.knhd_tv');
-
-const htmlRegis_knhd_1_0_0 = `
-<div class="regis_knhd_1_0_0 modal" id="regis_knhd_1_0_0__modal" style="display: flex;">
-    <div class="modal-bg" onclick="removeRegis_knhd_1_0_0()"></div>
+const htmlRegis_knhd_1_2_0 = `
+<div class="regis_knhd_1_2_0 modal" id="regis_knhd_1_2_0__modal" style="display: flex;">
+    <div class="modal-bg" onclick="removeRegis_knhd_1_2_0()"></div>
     <div class="modal-box animate-opacity">
-        <div class="regis_knhd_1_0_0__close modal-close" onclick="removeRegis_knhd_1_0_0()">×</div>
-        <div class="regis_knhd_1_0_0__body">
-            <div class="regis_knhd_1_0_0__title">Quý khách để lại thông tin để nhận ưu đãi tốt nhất</div>
-            <form class="regis_knhd_1_0_0__form" id="regis_knhd_1_0_0__form">
+        <div class="regis_knhd_1_2_0__close modal-close" onclick="removeRegis_knhd_1_2_0()">×</div>
+        <div class="regis_knhd_1_2_0__body">
+            <div class="regis_knhd_1_2_0__banner">
+                <img width="423" height="112" src="Popup/regis_knhd_1_2_0/images/banner.jpg" alt="">
+            </div>
+            <form class="regis_knhd_1_2_0__form" id="regis_knhd_1_2_0__form">
                 <div style="display: none;">
                     <input name="iemail" value="no@email.benhvienthammykangnam.vn">
                     <input type="email" name="email">
@@ -18,20 +18,20 @@ const htmlRegis_knhd_1_0_0 = `
                     <input name="website" value="">
                     <input name="location" value="">
                 </div>
-                <div class="regis_knhd_1_0_0__control form-group">
-                    <input type="text" name="iname" class="regis_knhd_1_0_0__input" placeholder="Họ tên">
+                <div class="regis_knhd_1_2_0__control form-group">
+                    <input type="text" name="iname" class="regis_knhd_1_2_0__input" placeholder="Họ tên">
                     <div class="form-message"></div>
                 </div>
-                <div class="regis_knhd_1_0_0__control form-group">
-                    <input type="text" name="imob" class="regis_knhd_1_0_0__input" placeholder="Số điện thoại">
+                <div class="regis_knhd_1_2_0__control form-group">
+                    <input type="text" name="imob" class="regis_knhd_1_2_0__input" placeholder="Số điện thoại">
                     <div class="form-message"></div>
                 </div>
-                 <div class="regis_knhd_1_2_0__control form-group" style="display: none">
+                <div class="regis_knhd_1_2_0__control form-group" style="display: none">
                     <input type="text" name="itext" class="regis_knhd_1_2_0__input">
                     <div class="form-message"></div>
                 </div>
-                <div class="regis_knhd_1_0_0__control form-group">
-                    <select class="regis_knhd_1_0_0__input" name="iservice">
+                <div class="regis_knhd_1_2_0__control form-group">
+                    <select class="regis_knhd_1_2_0__input" name="iservice">
                         <option value="">Dịch vụ</option>
                         <option value="nâng mũi">Nâng mũi</option>
                         <option value="cắt mí">Cắt mí</option>
@@ -44,8 +44,8 @@ const htmlRegis_knhd_1_0_0 = `
                     </select>
                     <div class="form-message"></div>
                 </div>
-                <div class="regis_knhd_1_0_0__submit">
-                    <input type="submit" value="Nhận ưu đãi">
+                <div class="regis_knhd_1_2_0__submit">
+                    <input type="submit" value="ĐẶT LỊCH TƯ VẤN NGAY">
                 </div>
             </form>
         </div>
@@ -53,20 +53,21 @@ const htmlRegis_knhd_1_0_0 = `
 </div>
 `;
 
-const removeRegis_knhd_1_0_0 = () => {
-    document.querySelector('#regis_knhd_1_0_0__modal').remove();
+const removeRegis_knhd_1_2_0 = () => {
+    document.querySelector('#regis_knhd_1_2_0__modal').remove();
 }
 
-btnRegis_knhd_1_0_0.addEventListener('click', () => {
-    document.body.insertAdjacentHTML('beforeend', htmlRegis_knhd_1_0_0);
+const btnRegis_knhd_1_2_0 = document.querySelector('.knhd_regis');
+btnRegis_knhd_1_2_0.addEventListener('click', () => {
+    document.body.insertAdjacentHTML("beforeend", htmlRegis_knhd_1_2_0);
     // thao tác form
-    document.querySelector('#regis_knhd_1_0_0__form input[name="first_link"]').value = document.URL;
-    document.querySelector('#regis_knhd_1_0_0__form input[name="website"]').value = document.referrer;
+    document.querySelector('#regis_knhd_1_2_0__form input[name="first_link"]').value = document.URL;
+    document.querySelector('#regis_knhd_1_2_0__form input[name="website"]').value = document.referrer;
     getLocation().then((data) => {
-        document.querySelector('#regis_knhd_1_0_0__form input[name="location"]').value = data.city;
+        document.querySelector('#regis_knhd_1_2_0__form input[name="location"]').value = data.city;
     });
     Validator({
-        form: "#regis_knhd_1_0_0__form",
+        form: "#regis_knhd_1_2_0__form",
         errorSelector: ".form-message",
         formGroupSelector: ".form-group",
         rules: [
@@ -74,15 +75,15 @@ btnRegis_knhd_1_0_0.addEventListener('click', () => {
             Validator.isRequired('input[name="imob"]'),
             Validator.isMobile('input[name="imob"]'),
             Validator.isRequired('select[name="iservice"]'),
-            Validator.addInput("#regis_knhd_1_0_0__form input[name='itext']", () => "Dịch vụ quan tâm: " + document.querySelector('#regis_knhd_1_0_0__form select[name="iservice"]').value),
+            Validator.addInput("#regis_knhd_1_2_0__form input[name='itext']", () => "Dịch vụ quan tâm: " + document.querySelector('#regis_knhd_1_2_0__form select[name="iservice"]').value),
         ],
         onSubmit: function (data) {
-            if (getValue('#regis_knhd_1_0_0__form input[name="email"]') === '') {
+            if (getValue('#regis_knhd_1_2_0__form input[name="email"]') === '') {
                 console.log(data);
                 sendForm(data, "/dang-ky-thanh-cong");
             }
             // parame: Form, Input
-            disableButton('#regis_knhd_1_0_0__form', '#regis_knhd_1_0_0__form input[type="submit"]');
+            disableButton('#regis_knhd_1_2_0__form', '#regis_knhd_1_2_0__form input[type="submit"]');
         },
     });
 })
