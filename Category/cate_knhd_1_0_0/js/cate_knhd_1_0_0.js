16bb83389e55cd73084d397109d877ea06f07bcf
nguyenquocviet1002 <vietnguyen10022001@gmail.com>
2024-10-03 09:12:54 +0700

    update

--- Category/cate_knhd_1_0_0/js/cate_knhd_1_0_0.js
+++ Category/cate_knhd_1_0_0/js/cate_knhd_1_0_0.js
@@ -7,3 +7,21 @@ btnsCate_knhd_1_0_0.forEach(item => {
         item.classList.add('active');
     };
 });
+
+const activeMobile = () => {
+    const windowWidth = window.innerWidth;
+    if(windowWidth < 768){
+        btnsCate_knhd_1_0_0.forEach(btn => {
+            btn.addEventListener('click', e => {
+                if(btn.classList.contains('active')){
+                    e.preventDefault();
+                }
+                btnsCate_knhd_1_0_0.forEach(item => {
+                    item.classList.toggle('show');
+                })
+            })
+        })
+    }
+};
+activeMobile();
+window.addEventListener('resize', () => activeMobile())
